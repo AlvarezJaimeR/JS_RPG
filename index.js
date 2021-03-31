de8ed446cb16a960f761fbe02611b6c71cc88ccd
AlvarezJaimeR <jrralvarez@gmail.com>
2021-03-31 19:33:46 -0400

    added a hero and foe class. created the hero and multiple foes.

--- index.js
+++ index.js
@@ -1,84 +1,58 @@
 "use strict"
 
-alert('You are Hercules, the greatest of the Greek Heroes! Prepare for battle!');
-runGame();
+class Hero{
+    constructor(name, health, attackPower, attack){
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+        this.attack = attack;
+    }
 
-//Hercules stats
-function hercules(){
-    let hercules = {
-        herculesHealth: 1000,
-        herculesAttackPower: 50,
-        herculesAttacks: {
-            attacks: ['Sword Slash','Double Knee Drop','Tombstone Piledriver']
+    //Hercules attack prompt
+    chooseAttack(){
+        alert('Choose an attack: ');
+        let attack = prompt('1, 2, or 3');
+        switch(attack){
+            case '1':
+                console.log(hercules.herculesAttacks.attacks[0]);
+                break;
+            case '2':
+                console.log(hercules.herculesAttacks.attacks[1]);
+                break;
+            case '3':
+                console.log(hercules.herculesAttacks.attacks[2]);
+                break;
         }
-    };return hercules;
-}
-
-//Nemean Lion stats
-function nemeanLion(){
-    let nemeanLion = {
-        lionHealth: 500,
-        lionAttackPower: 25,
-        lionAttacks: {
-            attacks: ['Rawr', 'Claw Slash', 'Head Bite']
-        }
-    };return nemeanLion;
-}
-
-//Lernaen Hydra stats
-function lerneanHydra(){
-    let lernaenHydra = {
-        hydraHealth: 500,
-        hydraAttackPower: 40,
-        hydraAttacks: {
-            attacks: ['Bite', 'Tail Whip', 'Flamethrower']
-        }
-    };return lernaenHydra
-}
-
-//Cerberus stats
-function cerberus(){
-    let cerberus = {
-        cerberusHealth: 5000,
-        cerberusAttackPower: 100,
-        cerberusAttacks: {
-            attacks: ['Headbutt', 'Fireball', 'Rock Throw']
-        }
-    }; return cerberus;
-}
-
-//Hercules attack prompt
-function attack(hercules){
-    alert('Choose an attack: ')
-    let attack = prompt('1, 2, or 3');
-    switch(attack){
-        case '1':
-            console.log(hercules.herculesAttacks.attacks[0]);
-            break;
-        case '2':
-            console.log(hercules.herculesAttacks.attacks[1]);
-            break;
-        case '3':
-            console.log(hercules.herculesAttacks.attacks[2]);
-            break;
     }
 }
 
-//Foes attack randomize
-function lionRandomAttacks(){
+class Foe{
+    constructor(name, health, attackPower, attack){
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+        this.attack = attack;
+    }
 
+    randomAttack(){
+        let randomAttack = this.attack[Math.floor(Math.random()*this.attack.length)];
+        console.log(this.name + ' will use ' + randomAttack);
+        return randomAttack;
+    }
 }
 
-function hydraRandomAttacks(){
+//Hercules stats
+let hercules = new Hero('Hercules', 4000, 100, ['Sword Slash','Double Knee Drop','Tombstone Piledriver']);
 
-}
+//Nemean Lion stats
+let nemeanLion = new Foe('Nemean Lion', 500, 25, ['Rawr', 'Claw Slash', 'Head Bite']);
 
-function cerberusRandomAttacks(){
+//Lernaen Hydra stats
+let lernaenHydra = new Foe('Lernaen Hydra', 600, 40, ['Bite', 'Tail Whip', 'Flamethrower']);
 
-}
+//Cerberus stats
+let cerberus = new Foe('Cerberus', 3000, 80, ['Headbutt', 'Fireball', 'Rock Throw']);
 
-//Main function to run the game
-function runGame(){
-    let herculesStats = hercules();
-    attack(herculesStats);
-}
+alert('You are Hercules, the greatest of the Greek Heroes! Prepare for battle!');
+hercules.chooseAttack();
+this.randomAttack();
