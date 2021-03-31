49ae0775ef780c5ed56faad502787a3a3035a1c0
AlvarezJaimeR <jrralvarez@gmail.com>
2021-03-31 19:57:16 -0400

    cleaned up the hero attack switch case. user is able to choose an attack.

--- index.js
+++ index.js
@@ -10,18 +10,28 @@ class Hero{
 
     //Hercules attack prompt
     chooseAttack(){
-        alert('Choose an attack: ');
-        let attack = prompt('1, 2, or 3');
-        switch(attack){
-            case '1':
-                console.log(hercules.herculesAttacks.attacks[0]);
-                break;
-            case '2':
-                console.log(hercules.herculesAttacks.attacks[1]);
-                break;
-            case '3':
-                console.log(hercules.herculesAttacks.attacks[2]);
-                break;
+        alert('A foe is heading towards you. Choose an attack:');
+        let attack = prompt('1 - Sword Slash 2 - Double Knee Drop 3 - Tombstone Piledriver');
+        let check = false;
+        while (check == false){
+            switch(attack){
+                case '1':
+                    console.log(this.name + ' will use ' + hercules.attack[0]);
+                    check = true;
+                    return hercules.attack[0];
+                case '2':
+                    console.log(this.name + ' will use ' + hercules.attack[1]);
+                    check = true;
+                    return hercules.attack[1];
+                case '3':
+                    console.log(this.name + ' will use ' + hercules.attack[2]);
+                    check = false;
+                    return hercules.attack[2];
+                default: 
+                    alert('Choose one of the main attacks');
+                    attack = prompt('1 - Sword Slash 2 - Double Knee Drop 3 - Tombstone Piledriver')
+                    break;
+            }
         }
     }
 }
@@ -41,18 +51,25 @@ class Foe{
     }
 }
 
+class Game{
+    constructor(){
+    }
+    runGame(){
+        alert('You are Hercules, the greatest of the Greek Heroes! Prepare for battle!');
+        hercules.chooseAttack();
+        nemeanLion.randomAttack();
+    }
+}
+
 //Hercules stats
 let hercules = new Hero('Hercules', 4000, 100, ['Sword Slash','Double Knee Drop','Tombstone Piledriver']);
-
 //Nemean Lion stats
 let nemeanLion = new Foe('Nemean Lion', 500, 25, ['Rawr', 'Claw Slash', 'Head Bite']);
-
 //Lernaen Hydra stats
 let lernaenHydra = new Foe('Lernaen Hydra', 600, 40, ['Bite', 'Tail Whip', 'Flamethrower']);
-
 //Cerberus stats
 let cerberus = new Foe('Cerberus', 3000, 80, ['Headbutt', 'Fireball', 'Rock Throw']);
 
-alert('You are Hercules, the greatest of the Greek Heroes! Prepare for battle!');
-hercules.chooseAttack();
-this.randomAttack();
+
+let game = new Game();
+game.runGame();
