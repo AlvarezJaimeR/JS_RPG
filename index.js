bdbe3fc5397be8e208594b61ce3f591331143bf1
AlvarezJaimeR <jrralvarez@gmail.com>
2021-04-01 20:37:08 -0500

    made the function name the same for both foe and hero.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Hero extends Character{
     }
 
     //Hercules attack prompt
-    chooseAttack(){
+    attack(){
         let attack = prompt('Choose an attack: 1 - Sword Slash 2 - Double Knee Drop 3 - Tombstone Piledriver');
         let check = false;
         while (check == false){
@@ -45,7 +45,7 @@ class Foe extends Character{
         super(name, health, attackPower, attack);
     }
 
-    randomAttack(){
+    attack(){
         let randomAttack = this.attack[Math.floor(Math.random()*this.attack.length)];
         console.log(this.name + ' will use ' + randomAttack);
         return randomAttack;
@@ -113,8 +113,8 @@ class Game{
 
     foeFight(foe){
         while (this.hercules.health > 0 || foe.health > 0){
-            this.hercules.chooseAttack();
-            foe.randomAttack();
+            this.hercules.attack();
+            foe.attack();
             this.hercules.health -= foe.attackPower;
             foe.health -= this.hercules.attackPower;
             console.log(this.hercules.name + ' only has ' + this.hercules.health + ' '
